f8ab981de6852ccae6e5f92ec5fc34fcb363d1c7
Jamie Moore <jamiemoore2689@gmail.com>
2022-03-28 08:41:40 +0100

    animated home hero and linked project cards

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,11 +2,37 @@ import React from 'react';
 import './Hero.css';
 
 function Hero(props) {
+
+
+    function addScrollHandle() {
+        window.addEventListener('scroll', handleScroll);
+    }
+
+    function handleScroll() {
+        const bottomCircle = document.querySelector(`.home-hero__circ-bottom`);
+        const topCircle = document.querySelector(`.home-hero__circ-top`);
+        const title = document.querySelector(`.home-hero__title`);
+        const text = document.querySelector(`.home-hero__text`);
+        let amountScrolled = window.pageYOffset;
+        topCircle.style.animationFillMode = `unset`;
+        topCircle.style.transform = `translateX(${amountScrolled / 4 }px)`;
+        bottomCircle.style.animationFillMode = `unset`;
+        bottomCircle.style.transform = `translateX(-${amountScrolled / 8 }px)`;
+        title.style.opacity = '1';
+        title.style.animationFillMode = `unset`;
+        title.style.transform = `translateX(-${amountScrolled / 6 }px)`;
+        text.style.animationFillMode = `unset`;
+        text.style.opacity = '1';
+        text.style.transform = `translateX(${amountScrolled / 6 }px)`;
+    }
+
     return (
         <section className="home-hero">
             <div className="home-hero__content">
                 {props.title && <h1 className="home-hero__title">{props.title}</h1>}
                 {props.tagline && <h2 className="home-hero__text">{props.tagline}</h2>}
+                <span className="home-hero__circ-top"></span>
+                <span className="home-hero__circ-bottom" onAnimationEnd={addScrollHandle}></span>
             </div>
         </section>
     );
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -3,14 +3,14 @@ import './Project.css';
 
 function Project(props) {
     return (
-        <div className="project-card">
+        <a className="project-card" href={props.link}>
             {props.img && <img src={props.img} className="project-card__img" alt="" />}
             <div className="project-card__content">
                 {props.title && <h2 className="project-card__title">{props.title}</h2>}
                 {props.excerpt && <p className="project-card__text">{props.excerpt}</p>}
-                {props.link && <a href={props.link} className="project-card__link">View project</a>}
+                {props.link && <span className="project-card__link">View project</span>}
             </div>
-        </div>
+        </a>
 
     );
 }
