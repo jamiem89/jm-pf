048a904935856ec7e711fca99fdf5fd5ed62715a
Jamie Moore <jamiemoore2689@gmail.com>
2022-03-31 21:44:26 +0100

    fixed some live urls

--- src/components/Projects/ProjectsData.js
+++ src/components/Projects/ProjectsData.js
@@ -3,28 +3,28 @@ const ProjectsData = [
         id: 1,
         title: 'Medical Architecture',
         excerpt: 'Clean modern website for London-based architects. Built on WordPress and utilizing ACF.',
-        img: '/images/medical-architecture.jpg',
+        img: 'https://jamiem89.github.io/images/medical-architecture.jpg',
         link: 'https://medicalarchitecture.com',
     },
     {
         id: 2,
         title: 'Transform Festival',
         excerpt: 'The biggest independent festival of theatre and dance in the UK needed a loud, brash website. Built on WordPress.',
-        img: '/images/transform.jpg',
+        img: 'https://jamiem89.github.io/images/transform.jpg',
         link: 'https://transformfestival.org'
     },
     {
         id: 3,
         title: 'The Tetley',
         excerpt: `The collaborative art space needed a robust architecture solution to make it's large amount of content easily accessible. Built on WordPress, with ACF and GSAP.`,
-        img: '/images/tetley.jpg',
+        img: 'https://jamiem89.github.io/images/tetley.jpg',
         link: 'https://thetetley.org/'
     },
     {
         id: 4,
         title: 'North Brewing Co.',
         excerpt: 'COMING SOON',
-        img: '/images/north-brewing.jpg'
+        img: 'https://jamiem89.github.io/images/north-brewing.jpg'
     }
 ];
 
